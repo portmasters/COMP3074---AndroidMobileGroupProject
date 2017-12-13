0f12007a94b016e4b343a17419584d755dbae137
Albert Nguyen <albert4industry@gmail.com>
2017-12-13 02:44:53 -0500

    Project Start
    Initial setup

---
+++ Flight-Booking-App/app/src/main/java/com/example/pc/flight_booking_app/acitivities/FlightBooking.java
@@ -0,0 +1,15 @@
+package com.example.pc.flight_booking_app.acitivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pc.flight_booking_app.R;
+
+public class FlightBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_booking);
+    }
+}
---
+++ Flight-Booking-App/app/src/main/java/com/example/pc/flight_booking_app/acitivities/HomeScreen.java
@@ -0,0 +1,15 @@
+package com.example.pc.flight_booking_app.acitivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pc.flight_booking_app.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
